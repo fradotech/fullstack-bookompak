c620218dfe39a96adcd49b7770ca5434a119939d
fradotech <fradotech.id@gmail.com>
2023-02-11 11:39:21 +0700

    Add action button table

--- client/Pages/User/columns.tsx
+++ client/Pages/User/columns.tsx
@@ -1,5 +1,8 @@
 import { UserResponse } from "@server/modules/iam/user/infrastructure/user.response";
 import { ColumnsType } from "antd/es/table";
+import React from "react";
+import { RowActionButtons } from "../../Components/Molecules/RowActionButtons/RowActionButtons";
+import { Route } from "../../Enums/Route";
 
 export const usersColumns: ColumnsType<UserResponse> = [
   {
@@ -22,35 +25,36 @@ export const usersColumns: ColumnsType<UserResponse> = [
     dataIndex: 'role',
     key: 'role',
   },
-  // {
-  //   title: 'Action',
-  //   key: 'action',
-  //   width: '150px',
-  //   render: (data) => {
-  //     data
-  //     return (
-  //       <RowActionButtons
-  //         actions={[
-  //           {
-  //             type: 'view',
-  //             href: `#`,
-  //             title: 'view',
-  //           },
-  //           {
-  //             type: 'edit',
-  //             href: `#`,
-  //             title: 'edit',
-  //           },
-  //           {
-  //             type: 'delete',
-  //             title: 'delete',
-  //             onClick: () => {
-  //               // TODO : handle delete function
-  //             },
-  //           },
-  //         ]}
-  //       />
-  //     )
-  //   },
-  // },
+  {
+    title: 'Action',
+    key: 'action',
+    width: '150px',
+    render: (data: UserResponse) => {
+      return (
+        <RowActionButtons
+          actions={[
+            {
+              type: 'view',
+              href: `${Route.User}/${data.id}`,
+              title: 'view',
+            },
+            {
+              type: 'edit',
+              title: 'edit',
+              onClick: () => {
+                alert('Tambah Fitur CRUD User?')
+              },
+            },
+            {
+              type: 'delete',
+              title: 'delete',
+              onClick: () => {
+                alert('Tambah Fitur CRUD User?')
+              },
+            },
+          ]}
+        />
+      )
+    },
+  },
 ];
